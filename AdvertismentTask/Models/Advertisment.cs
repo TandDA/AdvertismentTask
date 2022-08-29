bef36b849d92bd1fc5b2c9dd9eef9c6462246b52
TandDA <tanduevdmitriy@gmail.com>
2022-08-29 11:58:31 +0300

    В модель объявлени добавленно свойство IsAvailable

--- AdvertismentTask/Models/Advertisment.cs
+++ AdvertismentTask/Models/Advertisment.cs
@@ -3,6 +3,7 @@
     public class Advertisement
     {
         public int Id { get; set; }
+        public bool IsAvailable { get; set; } = false;
         public string? Image { get; set; }
         public string Title { get; set; } = null!;
         public string? Text { get; set; }
